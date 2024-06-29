2b6715362ce148538d47d64ad1f431862cf1a992
Steven Kristian <steven.kristian@gdn-commerce.com>
2024-06-29 22:32:32 +0700

    test: tadded testing for controller /member

--- go.mod
+++ go.mod
@@ -4,7 +4,9 @@ go 1.19
 
 require (
 	github.com/gin-gonic/gin v1.10.0
+	github.com/go-playground/assert/v2 v2.2.0
 	github.com/sirupsen/logrus v1.9.3
+	github.com/smartystreets/goconvey v1.8.1
 	gorm.io/driver/postgres v1.5.9
 	gorm.io/gorm v1.25.10
 )
@@ -20,6 +22,7 @@ require (
 	github.com/go-playground/universal-translator v0.18.1 // indirect
 	github.com/go-playground/validator/v10 v10.20.0 // indirect
 	github.com/goccy/go-json v0.10.2 // indirect
+	github.com/gopherjs/gopherjs v1.17.2 // indirect
 	github.com/jackc/pgpassfile v1.0.0 // indirect
 	github.com/jackc/pgservicefile v0.0.0-20221227161230-091c0ba34f0a // indirect
 	github.com/jackc/pgx/v5 v5.5.5 // indirect
@@ -27,6 +30,7 @@ require (
 	github.com/jinzhu/inflection v1.0.0 // indirect
 	github.com/jinzhu/now v1.1.5 // indirect
 	github.com/json-iterator/go v1.1.12 // indirect
+	github.com/jtolds/gls v4.20.0+incompatible // indirect
 	github.com/klauspost/cpuid/v2 v2.2.7 // indirect
 	github.com/kr/text v0.2.0 // indirect
 	github.com/leodido/go-urn v1.4.0 // indirect
@@ -35,6 +39,7 @@ require (
 	github.com/modern-go/reflect2 v1.0.2 // indirect
 	github.com/pelletier/go-toml/v2 v2.2.2 // indirect
 	github.com/rogpeppe/go-internal v1.12.0 // indirect
+	github.com/smarty/assertions v1.15.0 // indirect
 	github.com/twitchyliquid64/golang-asm v0.15.1 // indirect
 	github.com/ugorji/go/codec v1.2.12 // indirect
 	golang.org/x/arch v0.8.0 // indirect
---
+++ internal/controller/gin/member_routes_test.go
@@ -0,0 +1,102 @@
+package controller
+
+import (
+	"chat-app/domain"
+	"chat-app/dto"
+	"chat-app/internal/configuration"
+	"chat-app/internal/logging"
+	repository "chat-app/internal/repository/postgres"
+	"chat-app/internal/service"
+	"encoding/json"
+	"net/http"
+	"net/http/httptest"
+	"os"
+	"path/filepath"
+	"strings"
+	"testing"
+
+	"github.com/gin-gonic/gin"
+	"github.com/go-playground/assert/v2"
+	"gorm.io/gorm"
+)
+
+var testServer *gin.Engine
+var testRepository *gorm.DB
+
+func TestMain(m *testing.M) {
+	testSetup()
+	code := m.Run()
+	testTeardown()
+	os.Exit(code)
+}
+
+func testSetup() {
+	baseDir, err := findBaseDir()
+	if err != nil {
+		panic(err)
+	}
+
+	if err := configuration.InitializeWithSpecifiedWorkingDirectory(baseDir, "test"); err != nil {
+		logging.Log.Error(err)
+		panic(err)
+	}
+
+	if err := repository.InitializeConnection(); err != nil {
+		logging.Log.Error(err)
+		panic(err)
+	}
+	repository.InitializeRepositories(repository.DB)
+
+	service.InitializeService(repository.DB, repository.MemberRepository)
+
+	InitializeGinServer()
+	InitializeRoutes(Server, service.MemberService)
+	testServer = Server
+	testRepository = repository.DB
+}
+
+// findBaseDir searches for the base directory by looking for a known marker file
+func findBaseDir() (string, error) {
+	dir, err := os.Getwd()
+	if err != nil {
+		return "", err
+	}
+
+	// Look for a known marker file in parent directories
+	for {
+		if _, err := os.Stat(filepath.Join(dir, "go.mod")); !os.IsNotExist(err) {
+			return dir, nil
+		}
+
+		parent := filepath.Dir(dir)
+		if parent == dir {
+			break
+		}
+		dir = parent
+	}
+	return "", os.ErrNotExist
+}
+
+func testTeardown() {
+	testRepository.Exec("DELETE FROM ca_member")
+	testRepository.Exec("DELETE FROM ca_transaction")
+}
+
+func TestCreateMember(t *testing.T) {
+	requestBody := dto.CreateMemberRequest{
+		Name:     "test-username",
+		Email:    "test@gmail.com",
+		Gender:   "MALE",
+		Password: "test123",
+	}
+	jsonReqBody, _ := json.Marshal(requestBody)
+	request, _ := http.NewRequest(http.MethodPost, "/v1/member", strings.NewReader(string(jsonReqBody)))
+
+	httpRec := httptest.NewRecorder()
+	Server.ServeHTTP(httpRec, request)
+
+	var expectedMember domain.Member
+	testRepository.Raw("SELECT * FROM ca_member WHERE email = 'test@gmail.com'").Find(&expectedMember)
+	assert.Equal(t, http.StatusOK, httpRec.Code)
+	assert.Equal(t, domain.Member{}, expectedMember)
+}
