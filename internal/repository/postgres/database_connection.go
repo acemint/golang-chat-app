f80274efda40549099a56d4d2557ef496f370f8f
Steven Kristian <steven.kristian@gdn-commerce.com>
2024-06-29 13:53:40 +0700

    bugfix: unable to connect to DB due to wrong configuration key

--- internal/repository/postgres/database_connection.go
+++ internal/repository/postgres/database_connection.go
@@ -13,7 +13,19 @@ import (
 var DB *gorm.DB
 var MemberRepository *MemberRepositoryStruct
 
-func InitializePostgreSqlConnection() error {
+func InitializeConnection() error {
+	driver := setupDriver()
+	created_db, err := gorm.Open(driver, &gorm.Config{})
+	if err != nil {
+		return err
+	}
+	DB = created_db
+
+	initializeRepositories()
+	return nil
+}
+
+func setupDriver() gorm.Dialector {
 	dbConf := configuration.AppConfiguration.Database
 	dsn := fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%s sslmode=%s TimeZone=%s",
 		dbConf.Host,
@@ -23,14 +35,7 @@ func InitializePostgreSqlConnection() error {
 		strconv.Itoa(int(dbConf.Port)),
 		dbConf.SSLMode,
 		dbConf.TimeZone)
-
-	created_db, err := gorm.Open(postgres.New(postgres.Config{DSN: dsn, PreferSimpleProtocol: true}), &gorm.Config{})
-	if err != nil {
-		return err
-	}
-	DB = created_db
-	initializeRepositories()
-	return nil
+	return postgres.New(postgres.Config{DSN: dsn, PreferSimpleProtocol: true})
 }
 
 func initializeRepositories() {
